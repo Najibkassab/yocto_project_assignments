a8c0b5d3f0e2733a46ff2706e2704d8bef3476b4
Najibkassab <najeebkassab@hotmail.com>
2022-09-18 12:25:02 +0200

    first assignment

--- first assignment.c
+++ first assignment.c
@@ -4,12 +4,12 @@
 
 int main(){
 char input[100];
-
 printf("ready!! type anything you want:\r\n");
 while(1){
+printf("My_prompt>");
 fgets(input, sizeof(input), stdin);
 if(strcmp(input,"exit\n")!=0){
- printf("you said %s\r\n",input);
+ printf("My_prompt> you said %s",input);
 }else{
  printf("Good Bye!!\r\n");
  exit(0);
